23d5097cf8eae3e6c904adf108038a22f059f5eb
Channing <channing.phong@gmail.com>
2021-08-29 09:53:56 -0400

    Initial commit

---
+++ practice1.py
@@ -0,0 +1,7 @@
+# Personal Message Exercise
+firstName = ' John'
+lastName = 'Smith '
+firstName = firstName.lstrip()
+lastName = lastName.rstrip()
+message = f"Hello {firstName} {lastName}, \nhow is your day?"
+print(message)
