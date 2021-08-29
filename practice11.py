919c4460a7803c7e7b76d1c99a5f6371abdbc610
Channing <channing.phong@gmail.com>
2021-08-29 09:54:31 -0400

    Initial commit

---
+++ practice11.py
@@ -0,0 +1,25 @@
+age = 72
+if age < 2:
+    print("This person is a baby.")
+elif age <= 4:
+    print("This person is a toddler.")
+elif age <= 13:
+    print("This person is a kid.")
+elif age <= 20:
+    print("This person is a teenager.")
+elif age <= 65:
+    print("This person is an adult.")
+elif age >= 65:
+    print("This person is an elder.")
+
+fruits = ['apple', 'orange', 'banana']
+if 'apple' in fruits:
+    print('Apples are cool.')
+if 'strawberry' in fruits:
+    print('Strawberries are cool.')
+if 'orange' in fruits:
+    print('Oranges are cool.')
+if 'banana' in fruits:
+    print('Bananas are cool.')
+if 'blueberry' in fruits:
+    print('Blueberries are cool.')
