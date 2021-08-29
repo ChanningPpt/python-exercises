0927ddbb2cf61b681ed47d117b74398380274d31
Channing <channing.phong@gmail.com>
2021-08-29 09:54:12 -0400

    Initial commit

---
+++ practice5.py
@@ -0,0 +1,11 @@
+# Pizza and Animals Exercise
+
+pizzas = ['cheese', 'pepperoni', 'hawaiian']
+for pizza in pizzas:
+    print(f"{pizza.title()} pizza tastes good.")
+print("\nPizza tastes pretty good overall.\n")
+
+animals = ['lions', 'tigers', 'cougars']
+for animal in animals:
+    print(f"{animal.title()} are a type of cat")
+print("\nThese cats are very aggressive predators and can kill you.")
