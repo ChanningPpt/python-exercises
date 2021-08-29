edeaffefd8d7cb5243d4770f50c69f7ccba10e61
Channing <channing.phong@gmail.com>
2021-08-29 09:54:26 -0400

    Initial commit

---
+++ practice10.py
@@ -0,0 +1,10 @@
+# Alien Colors
+alienColor = 'green'
+if 'green' in alienColor:
+    print("Player has earned 5 Points!")
+elif 'yellow' in alienColor:
+    print("Player got 10 Points!")
+elif 'red' in alienColor:
+    print("Player got 15 Points!")
+else:
+    print("Player got 10 Points!")
