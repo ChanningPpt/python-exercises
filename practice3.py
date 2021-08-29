c966daddd1af4831440e2f0b2cede8029eb78127
Channing <channing.phong@gmail.com>
2021-08-29 09:54:04 -0400

    Initial commit

---
+++ practice3.py
@@ -0,0 +1,25 @@
+# Seeing The World Exercise
+
+places = ['bangkok', 'paris', 'london', 'dubai', 'nyc']
+
+# Show original list
+print(places)
+
+# Show sorted list
+print(sorted(places))
+
+print(places)
+
+print(sorted(places, reverse=True))
+
+print(places)
+
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
